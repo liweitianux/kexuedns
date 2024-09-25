fd873b28989b6ba601d441ed560098ca8ffdc61f
Aaron LI <aly@aaronly.me>
2024-09-25 16:11:57 +0800

    main: Add '-version' flag to print version info

--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
+	"os"
 
 	"kexuedns/dns"
 	"kexuedns/log"
 	"kexuedns/ui"
 )
+
+var version string // set by build flags
 
 func main() {
 	logLevel := flag.String("log-level", "info", "log level: debug/info/warn/error")
@@ -21,7 +24,13 @@ func main() {
 	port := flag.Int("port", 5553, "DNS listening UDP port")
 	httpAddr := flag.String("http-addr", "127.0.0.1", "HTTP webui address")
 	httpPort := flag.Int("http-port", 8053, "HTTP webui port")
+	showVersion := flag.Bool("version", false, "show version")
 	flag.Parse()
+
+	if *showVersion {
+		fmt.Printf("kexuedns %s\n", version)
+		os.Exit(0)
+	}
 
 	log.SetLevelString(*logLevel)
 	log.Infof("set log level to [%s]", *logLevel)
