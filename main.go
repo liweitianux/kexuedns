822a87a0b2ca27ad3ad6a4efbf473550b38a1afb
Aaron LI <aly@aaronly.me>
2024-10-15 06:47:05 +0800

    main: Implement -config-dir and -config-init

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"kexuedns/config"
 	"kexuedns/dns"
@@ -17,10 +18,15 @@ import (
 	"kexuedns/ui"
 )
 
+const progname = "kexuedns"
+
 var version string // set by build flags
 
 func main() {
 	logLevel := flag.String("log-level", "info", "log level: debug/info/warn/error")
+	configDir := flag.String("config-dir", "",
+		fmt.Sprintf("config directory (default \"${XDG_CONFIG_HOME}/%s\")", progname))
+	configInit := flag.Bool("config-init", false, "initialize with the default configs")
 	addr := flag.String("addr", "127.0.0.1", "DNS listening address")
 	port := flag.Int("port", 5553, "DNS listening UDP port")
 	httpAddr := flag.String("http-addr", "127.0.0.1", "HTTP webui address")
@@ -29,16 +35,40 @@ func main() {
 	flag.Parse()
 
 	if *showVersion {
-		fmt.Printf("kexuedns %s\n", version)
+		fmt.Printf("%s %s\n", progname, version)
 		os.Exit(0)
 	}
 
 	log.SetLevelString(*logLevel)
 	log.Infof("set log level to [%s]", *logLevel)
 
-	if err := config.Initialize(); err != nil {
-		panic(err)
+	if *configDir == "" {
+		if dir := os.Getenv("XDG_CONFIG_HOME"); dir == "" {
+			fmt.Printf("ERROR: ${XDG_CONFIG_HOME} required but missing\n")
+			os.Exit(1)
+		} else {
+			*configDir = filepath.Join(dir, progname)
+			log.Infof("use default config directory: %s", *configDir)
+		}
 	}
+
+	if *configInit {
+		if err := config.Initialize(*configDir); err != nil {
+			fmt.Printf("ERROR: failed to initialize config: %v\n", err)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
+
+	if err := config.Load(*configDir); err != nil {
+		fmt.Printf("ERROR: failed to load config: %v\n", err)
+		os.Exit(1)
+	}
+
+	conf := config.Get()
+	log.SetLevelString(conf.LogLevel)
+	log.Infof("set log level to [%s]", conf.LogLevel)
+
 	go func() {
 		listen := fmt.Sprintf("%s:%d", *addr, *port)
 		log.Infof("DNS service (UDP): %s", listen)
