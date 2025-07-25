72054c801d9a96e8b658f875a15e8df2473ae57f
Aaron LI <aly@aaronly.me>
2025-07-25 22:39:32 +0800

    api: Implement POST /start and /stop to start/stop forwarder

--- api/api.go
+++ api/api.go
@@ -7,9 +7,11 @@ package api
 
 import (
 	"net/http"
+	"net/netip"
 
 	"kexuedns/config"
 	"kexuedns/dns"
+	"kexuedns/log"
 )
 
 type ApiHandler struct {
@@ -19,16 +21,17 @@ type ApiHandler struct {
 	mux       *http.ServeMux
 }
 
-func NewApiHandler(forwarder *dns.Forwarder) *ApiHandler {
+func NewApiHandler() *ApiHandler {
 	h := &ApiHandler{
-		forwarder: forwarder,
+		forwarder: dns.NewForwarder(),
 		config:    config.Get(),
 		myip:      config.GetMyIP(),
 		mux:       http.NewServeMux(),
 	}
 	// NOTE: Patterns require Go 1.22.0+
+	h.mux.HandleFunc("POST /start", h.start)
+	h.mux.HandleFunc("POST /stop", h.stop)
 	h.mux.HandleFunc("GET /version", h.getVersion)
-	h.mux.HandleFunc("/", h.handleIndex)
 	return h
 }
 
@@ -36,10 +39,52 @@ func (h *ApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	h.mux.ServeHTTP(w, r)
 }
 
-func (h *ApiHandler) handleIndex(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("yo\n"))
+// Start the forwarder.
+// Input: nil
+// Return:
+// - 500: error
+// - 204: success
+func (h *ApiHandler) start(w http.ResponseWriter, r *http.Request) {
+	if r := h.config.Resolver; r == nil {
+		log.Warnf("no resolver configured yet")
+	} else {
+		resolver, err := dns.NewResolver(r.IP, r.Port, r.Hostname)
+		if err != nil {
+			log.Warnf("failed to create resolver: %+v, error: %v", r, err)
+		} else {
+			h.forwarder.SetResolver(resolver)
+			log.Infof("set resolver: %+v", r)
+		}
+	}
+
+	addr, err := netip.ParseAddr(h.config.ListenAddr)
+	if err != nil {
+		log.Errorf("invalid listen address: %s, error: %v", h.config.ListenAddr, err)
+		http.Error(w, "invalid address: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	addrport := netip.AddrPortFrom(addr, h.config.ListenPort)
+	pc, err := h.forwarder.Listen(addrport.String())
+	if err != nil {
+		log.Errorf("failed to listen at: %s, error: %v", addrport.String(), err)
+		http.Error(w, "listen failure: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	go h.forwarder.Serve(pc)
+	log.Infof("started forwarder at: %s", addrport.String())
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+// Stop the forwarder.
+// Input: nil
+// Return:
+// - 204: success
+func (h *ApiHandler) stop(w http.ResponseWriter, r *http.Request) {
+	h.forwarder.Stop()
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func (h *ApiHandler) getVersion(w http.ResponseWriter, r *http.Request) {
