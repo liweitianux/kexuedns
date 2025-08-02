2e2a4e90234092ddd909fa17e7ea0090a5be2259
Aaron LI <aly@aaronly.me>
2025-08-02 09:27:04 +0800

    util/dnstrie: Update tests to match code

--- util/dnstrie/dnstrie_test.go
+++ util/dnstrie/dnstrie_test.go
@@ -8,8 +8,6 @@
 package dnstrie
 
 import (
-	"bytes"
-	"slices"
 	"testing"
 )
 
@@ -50,63 +48,80 @@ func TestKey1(t *testing.T) {
 func TestZone1(t *testing.T) {
 	trie := &DNSTrie{}
 	zone := "abc.com"
+	value1, value2 := 42, 43
 
 	// Empty trie should just work.
-	trie.DeleteZone(zone)
-	if ok := trie.HasZone(zone); ok {
-		t.Errorf(`HasZone(%q) = %t; want false`, zone, ok)
+	if v, deleted := trie.DeleteZone(zone); v != nil || deleted {
+		t.Errorf(`DeleteZone(%q) = (%v, %t); want (nil, false)`, zone, v, deleted)
+	}
+	if v, ok := trie.GetZone(zone); v != nil || ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (nil, false)`, zone, v, ok)
 	}
 
-	trie.AddZone(zone)
-	if ok := trie.HasZone(zone); !ok {
-		t.Errorf(`HasZone(%q) = %t; want true`, zone, ok)
+	if v, updated := trie.AddZone(zone, value1); v != nil || updated {
+		t.Errorf(`AddZone(%q) = (%v, %t); want (nil, false)`, zone, v, updated)
+	}
+	if v, ok := trie.GetZone(zone); v == nil || !ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (%q, true)`, zone, v, ok, value1)
 	}
 
 	// Duplicate additions are ok.
-	trie.AddZone(zone)
-	if ok := trie.HasZone(zone); !ok {
-		t.Errorf(`HasZone(%q) = %t; want true`, zone, ok)
+	if v, updated := trie.AddZone(zone, value2); v != value1 || !updated {
+		t.Errorf(`AddZone(%q) = (%v, %t); want (%v, true)`, zone, v, updated, value1)
+	}
+	if v, ok := trie.GetZone(zone); v == nil || !ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (%q, true)`, zone, v, ok, value2)
 	}
 
-	trie.DeleteZone(zone)
-	if ok := trie.HasZone(zone); ok {
-		t.Errorf(`HasZone(%q) = %t; want false`, zone, ok)
+	if v, deleted := trie.DeleteZone(zone); v == nil || !deleted {
+		t.Errorf(`DeleteZone(%q) = (%v, %t); want (%q, true)`, zone, v, deleted, value2)
+	}
+	if v, ok := trie.GetZone(zone); v != nil || ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (nil, false)`, zone, v, ok)
 	}
 
 	// Duplicate deletions are ok.
-	trie.DeleteZone(zone)
-	if ok := trie.HasZone(zone); ok {
-		t.Errorf(`HasZone(%q) = %t; want false`, zone, ok)
+	if v, deleted := trie.DeleteZone(zone); v != nil || deleted {
+		t.Errorf(`DeleteZone(%q) = (%v, %t); want (nil, false)`, zone, v, deleted)
+	}
+	if v, ok := trie.GetZone(zone); v != nil || ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (nil, false)`, zone, v, ok)
 	}
 }
 
 func TestZone2(t *testing.T) {
 	trie := &DNSTrie{}
-	zone1 := "abc.com"
-	zone2 := "xyz.com"
+	zone1, value1 := "abc.com", 42
+	zone2, value2 := "xyz.com", 43
 
-	trie.AddZone(zone1)
-	if ok := trie.HasZone(zone1); !ok {
-		t.Errorf(`HasZone(%q) = %t; want true`, zone1, ok)
+	if v, updated := trie.AddZone(zone1, value1); v != nil || updated {
+		t.Errorf(`AddZone(%q) = (%v, %t); want (nil, false)`, zone1, v, updated)
 	}
-	if ok := trie.HasZone(zone2); ok {
-		t.Errorf(`HasZone(%q) = %t; want false`, zone2, ok)
+	if v, ok := trie.GetZone(zone1); v == nil || !ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (%q, true)`, zone1, v, ok, value1)
 	}
-
-	trie.AddZone(zone2)
-	if ok := trie.HasZone(zone1); !ok {
-		t.Errorf(`HasZone(%q) = %t; want true`, zone1, ok)
-	}
-	if ok := trie.HasZone(zone2); !ok {
-		t.Errorf(`HasZone(%q) = %t; want true`, zone2, ok)
+	if v, ok := trie.GetZone(zone2); v != nil || ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (nil, false)`, zone2, v, ok)
 	}
 
-	trie.DeleteZone(zone1)
-	if ok := trie.HasZone(zone1); ok {
-		t.Errorf(`HasZone(%q) = %t; want false`, zone1, ok)
+	if v, updated := trie.AddZone(zone2, value2); v != nil || updated {
+		t.Errorf(`AddZone(%q) = (%v, %t); want (nil, false)`, zone2, v, updated)
 	}
-	if ok := trie.HasZone(zone2); !ok {
-		t.Errorf(`HasZone(%q) = %t; want true`, zone2, ok)
+	if v, ok := trie.GetZone(zone1); v == nil || !ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (%q, true)`, zone1, v, ok, value1)
+	}
+	if v, ok := trie.GetZone(zone2); v == nil || !ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (%q, true)`, zone2, v, ok, value2)
+	}
+
+	if v, deleted := trie.DeleteZone(zone1); v == nil || !deleted {
+		t.Errorf(`DeleteZone(%q) = (%v, %t); want (%q, true)`, zone1, v, deleted, value1)
+	}
+	if v, ok := trie.GetZone(zone1); v != nil || ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (nil, false)`, zone1, v, ok)
+	}
+	if v, ok := trie.GetZone(zone2); v == nil || !ok {
+		t.Errorf(`GetZone(%q) = (%v, %t); want (%q, true)`, zone2, v, ok, value2)
 	}
 }
 
@@ -116,9 +131,8 @@ func TestMatch1(t *testing.T) {
 	// Empty trie should just work.
 	names := []string{"", ".", "com", "abc.com", "www.abc.com"}
 	for _, name := range names {
-		if key, ok := trie.Match(name); key != nil || ok {
-			t.Errorf(`Match(%q) = (%q, %t); want (nil, false)`,
-				name, key.String(), ok)
+		if v, ok := trie.Match(name); v != nil || ok {
+			t.Errorf(`Match(%q) = (%v, %t); want (nil, false)`, name, v, ok)
 		}
 	}
 }
@@ -126,47 +140,62 @@ func TestMatch1(t *testing.T) {
 func TestMatch2(t *testing.T) {
 	trie := &DNSTrie{}
 
-	zones := []string{"com", "xyz.", "abc.com", "xyz.net"}
-	for _, zone := range zones {
-		trie.AddZone(zone)
+	zones := []struct {
+		name  string
+		value int
+	}{
+		{name: "com", value: 1},
+		{name: "xyz.", value: 2},
+		{name: "abc.com", value: 3},
+		{name: "xyz.net", value: 4},
+	}
+	for _, z := range zones {
+		trie.AddZone(z.name, z.value)
 	}
 
 	items := []struct {
-		name       string
-		matchedKey Key
+		name    string
+		matched bool
+		value   int
 	}{
-		{name: "", matchedKey: nil},
-		{name: ".", matchedKey: nil},
-		{name: "net", matchedKey: nil},
-		{name: ".net", matchedKey: nil},
-		{name: ".xyz", matchedKey: NewKey("xyz")},
-		{name: ".org", matchedKey: nil},
-		{name: "com", matchedKey: NewKey("com")},
-		{name: "COM.", matchedKey: NewKey("com")},
-		{name: ".com", matchedKey: NewKey("com")},
-		{name: "cccom", matchedKey: nil},
-		{name: ".cccom", matchedKey: nil},
-		{name: "abc.net", matchedKey: nil},
-		{name: ".abc.net", matchedKey: nil},
-		{name: "xyz.com", matchedKey: NewKey("com")},
-		{name: "XYZ.COM.", matchedKey: NewKey("com")},
-		{name: ".XYZ.COM.", matchedKey: NewKey("com")},
-		{name: "abcxyz.com", matchedKey: NewKey("com")},
-		{name: "xyz.net", matchedKey: NewKey("xyz.net")},
-		{name: "abcxyz.net", matchedKey: nil},
-		{name: "abc.COM", matchedKey: NewKey("abc.com")},
-		{name: "ABC.com.", matchedKey: NewKey("abc.com")},
-		{name: "abcabc.com", matchedKey: NewKey("com")},
-		{name: "www.abc.com.", matchedKey: NewKey("abc.com")},
-		{name: "wwwxyz.abc.com", matchedKey: NewKey("abc.com")},
-		{name: "xyz.WWW.abc.com.", matchedKey: NewKey("abc.com")},
+		{name: "", matched: false},
+		{name: ".", matched: false},
+		{name: "net", matched: false},
+		{name: ".net", matched: false},
+		{name: ".xyz", matched: true, value: 2},
+		{name: ".org", matched: false},
+		{name: "com", matched: true, value: 1},
+		{name: "COM.", matched: true, value: 1},
+		{name: ".com", matched: true, value: 1},
+		{name: "cccom", matched: false},
+		{name: ".cccom", matched: false},
+		{name: "abc.net", matched: false},
+		{name: ".abc.net", matched: false},
+		{name: "xyz.com", matched: true, value: 1},
+		{name: "XYZ.COM.", matched: true, value: 1},
+		{name: ".XYZ.COM.", matched: true, value: 1},
+		{name: "abcxyz.com", matched: true, value: 1},
+		{name: "xyz.net", matched: true, value: 4},
+		{name: "abcxyz.net", matched: false},
+		{name: "abc.COM", matched: true, value: 3},
+		{name: "ABC.com.", matched: true, value: 3},
+		{name: "abcabc.com", matched: true, value: 1},
+		{name: "www.abc.com.", matched: true, value: 3},
+		{name: "wwwxyz.abc.com", matched: true, value: 3},
+		{name: "xyz.WWW.abc.com.", matched: true, value: 3},
 	}
 	for _, item := range items {
-		expected := (item.matchedKey != nil)
-		key, ok := trie.Match(item.name)
-		if !bytes.Equal(item.matchedKey, key) || ok != expected {
-			t.Errorf(`Match(%q) = (%q, %t); want (%q, %t)`,
-				item.name, key.String(), ok, item.matchedKey.String(), expected)
+		v, ok := trie.Match(item.name)
+		if item.matched {
+			if !ok || v != item.value {
+				t.Errorf(`Match(%q) = (%v, %t); want (%v, true)`,
+					item.name, v, ok, item.value)
+			}
+		} else {
+			if ok || v != nil {
+				t.Errorf(`Match(%q) = (%v, %t); want (nil, false)`,
+					item.name, v, ok)
+			}
 		}
 	}
 }
@@ -178,9 +207,19 @@ func TestExport(t *testing.T) {
 		t.Errorf(`Export() = %q; want []`, zones)
 	}
 
-	zones := []string{".", "Com", "xyz.", "ABC.com", "xyz.net", "www.ABC.com"}
+	zones := []struct {
+		name  string
+		value int
+	}{
+		{name: ".", value: 1},
+		{name: "Com", value: 2},
+		{name: "xyz.", value: 3},
+		{name: "ABC.com", value: 4},
+		{name: "xyz.net", value: 5},
+		{name: "www.ABC.com", value: 6},
+	}
 	for _, z := range zones {
-		trie.AddZone(z)
+		trie.AddZone(z.name, z.value)
 	}
 
 	zonesGot := trie.Export()
@@ -189,9 +228,12 @@ func TestExport(t *testing.T) {
 		t.Errorf(`Export() => %d zones; want %d`, len(zonesGot), len(zones))
 	}
 
-	slices.Sort(zonesGot)
-	slices.Sort(zones)
-	if !slices.Equal(zonesGot, zones) {
-		t.Errorf(`Export() = %+v; want %+v`, zonesGot, zones)
+	for _, z := range zones {
+		v, ok := zonesGot[z.name]
+		if !ok {
+			t.Errorf(`Export() missing zone %q`, z.name)
+		} else if v != z.value {
+			t.Errorf(`Export() zone %q wrong value %v; want %v`, z.name, v, z.value)
+		}
 	}
 }
