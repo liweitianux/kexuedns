7b96ae531218c1646467fcdab1579222894f3931
Aaron LI <aly@aaronly.me>
2025-08-07 15:14:56 +0800

    ui: Remove unused and unnecessary templates support
    We're implementing a RESTful API and separating the frontend from
    the backend, so there is no need to render HTML templates.

--- ui/embed.go
+++ ui/embed.go
@@ -9,18 +9,12 @@ package ui
 
 import (
 	"embed"
-	"fmt"
-	"html/template"
 	"io/fs"
 	"net/http"
-
-	"kexuedns/log"
 )
 
-//go:embed index.html static template/*.tmpl
+//go:embed index.html static
 var content embed.FS
-
-var templates *template.Template
 
 func ServeStatic() http.Handler {
 	staticFS, err := fs.Sub(content, "static")
@@ -33,22 +27,9 @@ func ServeStatic() http.Handler {
 func ServeIndex(w http.ResponseWriter, r *http.Request) {
 	data, err := content.ReadFile("index.html")
 	if err != nil {
-		http.Error(w, fmt.Sprintf("500 internal server error: %v", err),
+		http.Error(w, "500 internal server error: "+err.Error(),
 			http.StatusInternalServerError)
 		return
 	}
 	w.Write(data)
-}
-
-func GetTemplate(name string) *template.Template {
-	if templates == nil {
-		templates = template.Must(template.ParseFS(content, "template/*.tmpl"))
-		tt := []string{}
-		for _, t := range templates.Templates() {
-			tt = append(tt, t.Name())
-		}
-		log.Infof("parsed templates: %+v", tt)
-	}
-
-	return templates.Lookup(name)
 }
