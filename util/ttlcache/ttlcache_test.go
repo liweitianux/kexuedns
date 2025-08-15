641c61d31dc58f305c6e84f8135cee86aba09619
Aaron LI <aly@aaronly.me>
2025-08-15 17:21:34 +0800

    util/ttlcache: Supplement tests

--- util/ttlcache/ttlcache_test.go
+++ util/ttlcache/ttlcache_test.go
@@ -14,7 +14,109 @@ import (
 )
 
 func TestAdd1(t *testing.T) {
-	// TODO...
+	// No eviction.
+	ttl, ttl_11 := 10*time.Millisecond, 11*time.Millisecond
+	cache := New(ttl, 10*time.Second, nil)
+	defer cache.Close()
+
+	key := "hello"
+
+	// Empty set.
+	if v, ok := cache.Get(key); ok || v != nil {
+		t.Errorf(`Get(%q) = (%v, %t); want (nil, false)`, key, v, ok)
+	}
+	if v, ok := cache.Pop(key); ok || v != nil {
+		t.Errorf(`Pop(%q) = (%v, %t); want (nil, false)`, key, v, ok)
+	}
+	cache.Delete(key) // should work
+
+	val1, val2 := 1, 2
+	if err := cache.Add(key, val1, DefaultTTL); err != nil {
+		t.Errorf(`Set(%q) = %v; want nil`, key, err)
+	}
+	if err := cache.Add(key, val2, DefaultTTL); err != ErrKeyExists {
+		t.Errorf(`Set(%q) = %v; want ErrKeyExists`, key, err)
+	}
+
+	// Should get the old value.
+	if v, ok := cache.Get(key); !ok || v != val1 {
+		t.Errorf(`Get(%q) = (%v, %t); want (%v, true)`, key, v, ok, val1)
+	}
+
+	// Set() should overwrite the old value.
+	cache.Set(key, val2, DefaultTTL)
+	if v, ok := cache.Get(key); !ok || v != val2 {
+		t.Errorf(`Get(%q) = (%v, %t); want (%v, true)`, key, v, ok, val2)
+	}
+
+	// Expire it.
+	time.Sleep(11 * time.Millisecond)
+	if v, ok := cache.Get(key); ok || v != nil {
+		t.Errorf(`Get(%q) = (%v, %t); want (nil, false)`, key, v, ok)
+	}
+
+	// Test custom TTL.
+	val3 := 3
+	cache.Set(key, val3, ttl*2)
+	// Should not expire.
+	time.Sleep(ttl_11)
+	if v, ok := cache.Get(key); !ok || v != val3 {
+		t.Errorf(`Get(%q) = (%v, %t); want (%v, true)`, key, v, ok, val3)
+	}
+	time.Sleep(ttl_11)
+	// Now expired.
+	if v, ok := cache.Get(key); ok || v != nil {
+		t.Errorf(`Get(%q) = (%v, %t); want (nil, false)`, key, v, ok)
+	}
+}
+
+func TestPopDelete(t *testing.T) {
+	// No expiration.
+	cache := New(10*time.Second, 0, nil)
+	defer cache.Close()
+
+	key1, value1 := "hello", 1
+	key2, value2 := "world", 2
+	cache.Set(key1, value1, NoTTL)
+	cache.Set(key2, value2, NoTTL)
+
+	if v, ok := cache.Pop(key1); !ok || v == nil {
+		t.Errorf(`Pop(%q) = (%v, %t); want (%v, true)`, key1, v, ok, value1)
+	}
+	// Popped, so no result.
+	if v, ok := cache.Pop(key1); ok || v != nil {
+		t.Errorf(`Pop(%q) = (%v, %t); want (nil, false)`, key1, v, ok)
+	}
+	// Get() should return nil.
+	if v, ok := cache.Get(key1); ok || v != nil {
+		t.Errorf(`Get(%q) = (%v, %t); want (nil, false)`, key1, v, ok)
+	}
+
+	// Delete it and check.
+	cache.Delete(key2)
+	if v, ok := cache.Get(key2); ok || v != nil {
+		t.Errorf(`Get(%q) = (%v, %t); want (nil, false)`, key2, v, ok)
+	}
+}
+
+func TestNoTTL(t *testing.T) {
+	ttl := 10 * time.Millisecond
+	cache := New(ttl, 0, nil)
+	defer cache.Close()
+
+	key1, value1 := "hello", 1
+	key2, value2 := "world", 2
+	cache.Set(key1, value1, DefaultTTL)
+	cache.Set(key2, value2, NoTTL)
+
+	time.Sleep(ttl * 2)
+	// key1 expired, key2 not.
+	if v, ok := cache.Get(key1); ok || v != nil {
+		t.Errorf(`Get(%q) = (%v, %t); want (nil, false)`, key1, v, ok)
+	}
+	if v, ok := cache.Get(key2); !ok || v != value2 {
+		t.Errorf(`Get(%q) = (%v, %t); want (%v, true)`, key2, v, ok, value2)
+	}
 }
 
 func TestEviction1(t *testing.T) {
